9d71329047767f6b8e1a6b86d7786c548445f6b8
asd83101627 <18352613036@163.com>
2020-02-20 16:32:24 +0800

    2/20

--- 面向对象/instanceof.js
+++ 面向对象/instanceof.js
@@ -1,7 +1,7 @@
 //a instanceof A
-//nstanceof运算符用来判断一个构造函数的prototype属性所指向的对象是否存在另外一个要检测对象的原型链上
+//instanceof运算符用来判断一个构造函数的prototype属性所指向的对象是否存在另外一个要检测对象的原型链上
 
-function Person(name){
+function Person(name){i
     if(this instanceof Person){
         //this指向了当前实例，外部使用了关键字new
         this.name=name;
