de9ee40e614f822dcd31b9f4f18e464bda9a82cc
asd83101627 <18352613036@163.com>
2020-05-10 23:59:27 +0800

    5/10

---
+++ http/实践/webcache.js
@@ -0,0 +1,2 @@
+import Koa from 'koa'; import path from 'path';//静态资源中间件import resource from 'koa-static';const app = new Koa();const host = 'localhost';const port = 4396;app.use(resource(path.join(__dirname, './static')));
+app.listen(port, () => { console.log(`server is listen in ${host}:${port}`); });
